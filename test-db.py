919b3fa590a1ea6c707de241f6106537f1df0718
Billy J Hamlin <billy.hamlin@gmail.com>
2017-11-01 14:38:53 +0000

    Basic testing of db functionality

--- test-db.py
+++ test-db.py
@@ -16,7 +16,7 @@ c = {
     'race': 'dwarf',
     'level': [1],
     'class': ['wizard#d20srd'],
-    'class_features': [{'s12n': 'conjuration'}],
+    'class_features': [{'specialization': 'conjuration'}],
     'stats': { 
         'str': 10,
         'dex': 15,
@@ -30,15 +30,28 @@ c = {
             'ranks': 1,
             'check': 'int'
         }
-  }
+    },
+    'feats': {
+        'spell penetration': 1,
+    }
+}
 
 if not db._owner['bhamlin#6283']:
     db.insert(owner='bhamlin#6283', name='Covfefe', game=None, char=c)
     db.commit()
 
+for rec in db._owner['bhamlin#6283']:
+    q = rec['char']
+    q['level'][0] += 1
+    db.update(rec, char=q)
+    db.commit()
+
 for rec in (db('owner') == 'bhamlin#6283'):
-    print(rec)
-    print(json.dumps(rec['char']))
+    #print(rec)
+    #print(json.dumps(rec['char']))
+    print(rec['char']['name'], rec['char']['level'])
+
+print()
 
 # for rec in [rec for rec in db if rec['owner'] == 'bhamlin#6283']:
 #     print(rec)
